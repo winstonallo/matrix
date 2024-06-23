518e9f58588d1b5a0713f502aab0a0a53fc88468
winstonallo <arthurbiedchar@gmail.com>
2024-06-23 17:55:19 +0200

    cnn: initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod adder;
 pub mod boolean_eval;
+pub mod conjuctive_normal_form;
 pub mod gray_code;
 pub mod multiplier;
 pub mod negation_normal_form;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use ready_set_boole::adder;
 use ready_set_boole::boolean_eval;
+use ready_set_boole::conjuctive_normal_form;
 use ready_set_boole::gray_code;
 use ready_set_boole::multiplier;
 use ready_set_boole::negation_normal_form;
