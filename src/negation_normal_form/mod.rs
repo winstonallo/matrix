d92256f6812f5af3cd5f667fe0e9725a0f202cb4
winstonallo <arthurbiedchar@gmail.com>
2024-06-23 17:52:34 +0200

    add: bad input protection for nnf

--- src/negation_normal_form/mod.rs
+++ src/negation_normal_form/mod.rs
@@ -118,9 +118,12 @@ fn ast_to_string(node: ASTNode) -> String {
 }
 
 pub fn negation_normal_form(expression: &str) -> String {
-    let ast = rpn_to_ast(expression).unwrap();
-    let nnf_ast = to_nnf(ast);
-    ast_to_string(nnf_ast)
+    if let Some(ast) = rpn_to_ast(expression) {
+        let nnf_ast = to_nnf(ast);
+        ast_to_string(nnf_ast)
+    } else {
+        return "Invalid expression".to_string();
+    }
 }
 
 #[cfg(test)]
@@ -134,5 +137,12 @@ mod tests {
         assert_eq!(negation_normal_form("AB>"), "A!B|");
         assert_eq!(negation_normal_form("AB="), "AB&A!B!&|");
         assert_eq!(negation_normal_form("AB|C&!"), "A!B!&C!|");
+    }
+
+    #[test]
+    fn test_invalid_expression() {
+        assert_eq!(negation_normal_form("ABBB"), "Invalid expression");
+        assert_eq!(negation_normal_form(""), "Invalid expression");
+        assert_eq!(negation_normal_form("!AB"), "Invalid expression");
     }
 }
